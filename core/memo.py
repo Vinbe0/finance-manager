a21b8f1e81ea6f73c0055ee4b22eb0d197bbcebc
Vinbe0 <sofengedu@gmail.com>
2025-10-02 17:20:25 +0500

    lab-03.2

--- core/memo.py
+++ core/memo.py
@@ -3,9 +3,7 @@ from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def forecast_expenses(category: str, transactions: tuple[Transaction, ...], months: int) -> float:
-
     values = [abs(t.amount) for t in transactions if t.cat_id == category]
-
     if not values:
         return 0.0
 
